3473fe3960dc2c430ef2a44ed1e3e454022ab8b8
DataNath <Nathan.p100@hotmail.co.uk>
2025-03-14 01:10:15 +0000

    Switched to headless

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ print(f"Attempting to add {url_count} sources from provided links.csv file...\n"
 # Initialise browser session
 
 with sync_playwright() as sp:
-    browser = sp.chromium.launch(headless=False, channel="chrome")
+    browser = sp.chromium.launch(headless=True, channel="chrome")
     context = browser.new_context(storage_state="state.json")
     page = context.new_page()
 
@@ -36,22 +36,22 @@ with sync_playwright() as sp:
         if i == is_first:
 
             new_notebook_button = page.get_by_role("button", name="Create new notebook")
-            new_notebook_button.wait_for(state="visible")
+            new_notebook_button.wait_for(state="attached")
             new_notebook_button.click()
 
         website_button = page.locator("span.mdc-evolution-chip__text-label", has_text="Website")
-        website_button.wait_for(state="visible")
+        website_button.wait_for(state="attached")
         website_button.click()
 
         website_url_input = page.locator("[formcontrolname='newUrl']")
-        website_url_input.wait_for(state="visible")
+        website_url_input.wait_for(state="attached")
         website_url_input.fill(u)
         page.keyboard.press("Enter")
 
         checkbox_container = page.locator(
             "div.single-source-container"
         ).last
-        checkbox_container.wait_for(state="visible")
+        checkbox_container.wait_for(state="attached")
 
         loading_spinner = checkbox_container.locator(".mat-mdc-progress-spinner")
 
@@ -60,15 +60,15 @@ with sync_playwright() as sp:
         checkbox = checkbox_container.locator(
             "input.mdc-checkbox__native-control.mdc-checkbox--selected"
         )
-        checkbox.wait_for(state="visible")
+        checkbox.wait_for(state="attached")
         expect(checkbox).not_to_have_attribute("ariaLabel", u)
 
-        page.wait_for_timeout(1000)
+        page.wait_for_timeout(1200)
 
         if i < is_last:
 
             add_source_button = page.get_by_role("button", name="Add source")
-            add_source_button.wait_for(state="visible")
+            add_source_button.wait_for(state="attached")
             add_source_button.click()
 
         print(f"Source {i+1}/{url_count} ({u}) added.")
@@ -81,8 +81,9 @@ with sync_playwright() as sp:
     title_box.fill(notebook_name)
     title_box.press("Enter")
 
-    print("Title updated!\n")
+    page.wait_for_timeout(1000)
 
+    print("Title updated!\n")
 
     browser.close()
 
@@ -92,7 +93,7 @@ elapsed = round(end - start)
 
 if elapsed > 59:
     minutes = elapsed // 60
-    seconds = elapsed & minutes
+    seconds = elapsed % 60
 
     print(f"Time elapsed: {minutes} minutes and {seconds} seconds.")
 
